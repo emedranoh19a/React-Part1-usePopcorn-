7f3cf5952078b5730413f7a26d7c17eaef44af15
@WSS-Eric <e-medrano@ws-sol.co.jp>
2024-10-18 13:56:01 +0900

    fix for netlify

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import {  useRef } from "react";
 import { useKey } from "./useKey";
 export default function Search({ query, onSetQuery }) {
   const inputEl = useRef(null);
